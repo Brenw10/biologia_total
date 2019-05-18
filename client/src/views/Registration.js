06245c03d6fc3e66a1481e591e02e7ec72805005
Brendon Willian de Carvalho Trindade Mota <bmota@its.jnj.com>
2019-05-18 00:16:51 -0300

    Searching on registration

--- client/src/views/Registration.js
+++ client/src/views/Registration.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import RegistrationTable from '../components/RegistrationTable';
-import { Paper } from '@material-ui/core';
+import { Paper, TextField } from '@material-ui/core';
 import student from '../services/student';
 import StudentCoursesEdition from '../components/StudentCoursesEdition';
 
 export default class Registration extends React.Component {
   state = {
+    allStudents: [],
     students: [],
     student: {},
     open: false,
+    search: '',
   }
   componentDidMount() {
     this.refresh();
@@ -16,7 +18,15 @@ export default class Registration extends React.Component {
   refresh() {
     student
       .getAll()
-      .then(students => this.setState({ students }));
+      .then(allStudents => this.setState({ students: allStudents, allStudents }))
+      .then(() => this.onSearch(this.state.search));
+  }
+  onSearch(value) {
+    const search = value.toLowerCase();
+    const students = this.state.allStudents.filter(student =>
+      student.name.toLowerCase().indexOf(search) >= 0 || student.email.toLowerCase().indexOf(search) >= 0
+    );
+    this.setState({ search: value, students });
   }
   onCloseDialog() {
     this.setState({ open: false });
@@ -25,6 +35,16 @@ export default class Registration extends React.Component {
   render() {
     return (
       <Paper className="container">
+        <TextField
+          label="Procurar"
+          placeholder="Digite nome ou email"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{ shrink: true }}
+          helperText="Busque por algum aluno"
+          value={this.state.search}
+          onChange={event => this.onSearch(event.target.value)}
+        />
         <RegistrationTable
           students={this.state.students}
           onClickEdit={student => this.setState({ open: true, student })}
