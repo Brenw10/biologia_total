62a70c4ef3185d1ee174fe7dba24dc3623003afc
Brendon Willian de Carvalho Trindade Mota <bmota@its.jnj.com>
2019-05-17 22:07:29 -0300

    Added search

--- client/src/components/StudentTable.js
+++ client/src/components/StudentTable.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import { Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton, TextField } from '@material-ui/core';
+import { Table, TableHead, TableRow, TableCell, TableBody, IconButton } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import PropTypes from 'prop-types';
 
 export default class StudentTable extends React.Component {
   render() {
     return (
-      <Paper className="container">
-        <TextField
-          label="Procurar"
-          placeholder="Digite nome ou email"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{ shrink: true }}
-          helperText="Busque por algum aluno"
-        />
-        <Table>
-          <TableHead>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Data de Nascimento</TableCell>
+            <TableCell align="center">Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        {this.props.students.map((row, key) => (
+          <TableBody key={key}>
             <TableRow>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Email</TableCell>
-              <TableCell align="center">Data de Nascimento</TableCell>
-              <TableCell align="center">Ações</TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.birthday ? row.birthday : 'N/A'}</TableCell>
+              <TableCell align="center">
+                <IconButton onClick={() => this.props.onClickEdit(row)}>
+                  <EditIcon />
+                </IconButton>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          {this.props.students.map((row, key) => (
-            <TableBody key={key}>
-              <TableRow>
-                <TableCell align="center">{row.name}</TableCell>
-                <TableCell align="center">{row.email}</TableCell>
-                <TableCell align="center">{row.birthday ? row.birthday : 'N/A'}</TableCell>
-                <TableCell align="center">
-                  <IconButton onClick={() => this.props.onClickEdit(row)}>
-                    <EditIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          ))}
-        </Table>
-      </Paper>
+          </TableBody>
+        ))}
+      </Table>
     );
   }
 }
--- client/src/views/Student.js
+++ client/src/views/Student.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import StudentTable from '../components/StudentTable';
-import { Fab } from '@material-ui/core';
+import { Fab, TextField, Paper } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import StudentEdition from '../components/StudentEdition';
 import student from '../services/student';
@@ -9,7 +9,9 @@ export default class Student extends React.Component {
   state = {
     dialog: false,
     student: {},
+    allStudents: [],
     students: [],
+    search: '',
   }
   componentDidMount() {
     this.refresh();
@@ -17,7 +19,15 @@ export default class Student extends React.Component {
   refresh() {
     student
       .getAll()
-      .then(students => this.setState({ students }));
+      .then(allStudents => this.setState({ allStudents, students: allStudents }))
+      .then(() => this.onSearch(this.state.search));
+  }
+  onSearch(value) {
+    const search = value.toLowerCase();
+    const students = this.state.allStudents.filter(student =>
+      student.name.toLowerCase().indexOf(search) >= 0 || student.email.toLowerCase().indexOf(search) >= 0
+    );
+    this.setState({ search: value, students });
   }
   onCloseDialog() {
     this.setState({ dialog: false });
@@ -26,10 +36,22 @@ export default class Student extends React.Component {
   render() {
     return (
       <div>
-        <StudentTable
-          onClickEdit={student => this.setState({ dialog: true, student })}
-          students={this.state.students}
-        />
+        <Paper className="container">
+          <TextField
+            label="Procurar"
+            placeholder="Digite nome ou email"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{ shrink: true }}
+            helperText="Busque por algum aluno"
+            value={this.state.search}
+            onChange={event => this.onSearch(event.target.value)}
+          />
+          <StudentTable
+            onClickEdit={student => this.setState({ dialog: true, student })}
+            students={this.state.students}
+          />
+        </Paper>
         <StudentEdition
           open={this.state.dialog}
           student={this.state.student}
